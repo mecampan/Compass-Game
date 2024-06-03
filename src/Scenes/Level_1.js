963fdafe01fb01cd003e6e84b7ea2dc7dbe694d9
mecampan <bleachfan412@gmail.com>
2024-06-03 11:21:35 -0700

    had to rename some map parts. Map works now

--- src/Scenes/Level_1.js
+++ src/Scenes/Level_1.js
@@ -2,13 +2,26 @@ class Level_1 extends Phaser.Scene {
     constructor() {
         super("level1Scene");
     }
+    preload() {
+
+    }
 
     init() {
         
     }
 
     create() {
-        
+        // Create a new tilemap which uses 16x16 tiles, and is 40 tiles wide and 25 tiles tall
+        this.map = this.add.tilemap("dungeon_map", 30, 20, 16, 16);
+
+        // Add a tileset to the map
+        this.tileset = this.map.addTilesetImage("catacombs_tilemap", "tilemap_tiles");
+
+        // Create the layers
+        this.groundLayer = this.map.createLayer("groundLayer", this.tileset, 0, 0);
+
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        this.cameras.main.setZoom(2.0);
     }    
 
     update() {
--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -4,10 +4,14 @@ class Load extends Phaser.Scene {
     }
 
     preload() {
+        this.load.setPath("./assets/");
+
+        // Load tilemap information
+        this.load.image("tilemap_tiles", "catacombs_tilemap.png");                   // Packed tilemap
+        this.load.tilemapTiledJSON("dungeon_map", "dungeon_map.tmj");   // Tilemap in JSON
     }
 
     create() {
-
         // ...and pass to the next Scene
         this.scene.start("level1Scene");
     }
