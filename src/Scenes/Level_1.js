47f24e3cbcaac3b991e548c64ec8b2443a23e95e
mecampan <mecampan@ucsc.edu>
2024-06-10 16:53:18 -0700

    changes to map

--- src/Scenes/Level_1.js
+++ src/Scenes/Level_1.js
@@ -20,7 +20,6 @@ class Level_1 extends Phaser.Scene {
         // Create the layers
         this.groundLayer = this.map.createLayer("groundLayer", this.tileset, 0, 0);
         this.wallLayer = this.map.createLayer("wallLayer", this.tileset, 0, 0);
-        this.wallLayer.setCollisionByProperty({collides: true});
         // Layer that doesn't interact with light source
         this.frontLayer = this.map.createLayer("frontLayer", this.tileset, 0, 0); 
         this.spawnLayer = this.map.getObjectLayer('spawnLayer');
@@ -28,17 +27,14 @@ class Level_1 extends Phaser.Scene {
         // Fade in camera
         this.cameras.main.fadeIn(500, 0, 0, 0);
         // Enable collision for the wallLayer
-        //this.wallLayer.setCollisionByExclusion([-1]);
+        this.wallLayer.setCollisionByExclusion([-1]);
 
         this.scene.launch('hudScene'); // Start the UI scene
         this.HUD = this.scene.get('hudScene');
 
-       
-
         // Add collectable oil bottles:
-        this.oilBottles = [
-            this.physics.add.sprite(100, 50, 'oilBottle')
-        ];
+        this.oilBottles = [];
+        this.createOilBottles();
 
         // Set the bounds of the world to match the map dimensions
         this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
@@ -56,12 +52,7 @@ class Level_1 extends Phaser.Scene {
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
         this.cameras.main.setZoom(4.0);
 
-        this.createMaze(this.map, this.wallLayer, this.tileset);
-
-        // // Collision detection for the books:
-        // this.books.forEach(book => {
-        //     this.physics.add.overlap(this.player, book, this.collectBook, null, this);
-        // });
+        //this.createMaze(this.map, this.wallLayer, this.tileset);
 
         // Collision detection for the oil:
         this.oilBottles.forEach(bottle => {
@@ -74,19 +65,7 @@ class Level_1 extends Phaser.Scene {
 
         // Initialize enemies
         this.enemies = this.physics.add.group();
-
-        // Create enemies and add them to the group
-        this.enemyStartPoint1 = this.spawnLayer.objects.find(obj => obj.name === "enemySpawn1");
-        this.enemyStartPoint2 = this.spawnLayer.objects.find(obj => obj.name === "enemySpawn2");
-        this.enemyStartPoint3 = this.spawnLayer.objects.find(obj => obj.name === "enemySpawn3");
-
-        const enemy1 = new Enemy(this, this.enemyStartPoint1.x, this.enemyStartPoint1.y, 'evil_wizard', 'idle_01.png');
-        const enemy2 = new Enemy(this, this.enemyStartPoint2.x, this.enemyStartPoint2.y, 'evil_wizard', 'idle_01.png');
-        const enemy3 = new Enemy(this, this.enemyStartPoint3.x, this.enemyStartPoint3.y, 'evil_wizard', 'idle_01.png');
-
-        this.enemies.add(enemy1.sprite);
-        this.enemies.add(enemy2.sprite);   
-        this.enemies.add(enemy3.sprite);
+        this.spawnEnemies();
 
         // Player and Enemy Collider
         this.physics.add.overlap(this.player, this.enemies, this.playerEnemyCollision, null, this);
@@ -102,8 +81,6 @@ class Level_1 extends Phaser.Scene {
         // Add key listener for toggling debug mode
         this.input.keyboard.on('keydown-Y', this.toggleDebug, this);
         this.input.keyboard.on('keydown-SPACE', this.stunEnemy, this);
-        // Add key listener for teleporting player (checking maze)
-        this.input.keyboard.on('keydown-B', () => this.DBTeleport(this.player, "PlayerDBSpawn"), this);
     }
 
     update() {
@@ -130,7 +107,6 @@ class Level_1 extends Phaser.Scene {
 
     }
 
-    //This function is unfinished, it currently isn't working.
     createMaze(map, wallLayer, tileset) {
         const mazeWidth = 66;
         const mazeHeight = 42;
@@ -141,19 +117,7 @@ class Level_1 extends Phaser.Scene {
         const wallTileID = 1234;
         const mazeGenerator = new MazeGenerator(mazeWidth, mazeHeight, startX, startY, endX, endY, wallTileID, map, wallLayer, tileset);
         mazeGenerator.applyMaze();
-        // Ensure the layer is visible
         wallLayer.setVisible(true);
-    }
-
-    //debug function to check maze
-    DBTeleport(player, spawnName) {
-        const playerDBSpawn = this.map.findObject("spawnLayer", obj => obj.name === spawnName);
-        if (playerDBSpawn) {
-            player.setPosition(playerDBSpawn.x, playerDBSpawn.y);
-            console.log(`Player teleported to ${playerDBSpawn.x}, ${playerDBSpawn.y}`);
-        } else {
-            console.log(`Spawn point "${spawnName}" not found`);
-        }
     }
 
     createMinimap() {
@@ -170,7 +134,6 @@ class Level_1 extends Phaser.Scene {
     }
 
     spawnBooks(player) {
-        // Only spawn books if there are no books or if some books have been collected
         if (this.allBooksCollected === false) {
             this.player = player;
             const bookSpawnPoints = {
@@ -178,12 +141,12 @@ class Level_1 extends Phaser.Scene {
                 "spell_book2": ["bookB1", "bookB2", "bookB3", "bookB4", "bookB5", "bookB6", "bookB7", "bookB8", "bookB9", "bookB10", "bookB11"],
                 "spell_book3": ["bookC1", "bookC2", "bookC3"]
             };
-    
+
             Object.keys(bookSpawnPoints).forEach(bookType => {
                 const spawnPoints = bookSpawnPoints[bookType];
                 const randomSpawnPointName = Phaser.Utils.Array.GetRandom(spawnPoints);
                 const spawnPoint = this.map.findObject("spawnLayer", obj => obj.name === randomSpawnPointName);
-    
+
                 if (spawnPoint) {
                     const book = new Book(this, spawnPoint.x, spawnPoint.y, bookType);
                     this.books.push(book);
@@ -192,26 +155,28 @@ class Level_1 extends Phaser.Scene {
             });
         }
     }
-    
 
     collectBook(player, book) {
         book.collect();
-        this.collectedBooks++; // Increment book count
-        // Remove the collected book from the books array
+        this.collectedBooks++;
         this.books = this.books.filter(b => b !== book);
         this.minimapGraphics.clear();
-        // Check if all books are collected
         if (this.collectedBooks === 3) {
             console.log("All Books collected");
             this.allBooksCollected = true;
-            //this.collectedBooks = 0; // Reset book count
-            // Reinitialize books if needed
             this.spawnBooks(this.player);
         }
-        // Update the minimap
         this.updateMinimap();
-        // Update the UI scene with the collected book count
         this.scene.get('hudScene').events.emit('updateHud', this.collectedBooks);
+    }
+
+    createOilBottles() {
+        this.spawnLayer.objects.forEach(obj => {
+            if (obj.name === 'oilSpawn') {
+                const oilBottle = this.physics.add.sprite(obj.x, obj.y, 'oilBottle');
+                this.oilBottles.push(oilBottle);
+            }
+        });
     }
 
     collectOil(player, oil) {
@@ -219,14 +184,22 @@ class Level_1 extends Phaser.Scene {
         this.HUD.refilOil();
     }
 
+    spawnEnemies() {
+        this.spawnLayer.objects.forEach(obj => {
+            if (obj.name === 'enemySpawn') {
+                const enemy = new Enemy(this, obj.x, obj.y, 'evil_wizard', 'idle_01.png');
+                this.enemies.add(enemy.sprite);
+            }
+        });
+    }
+
     stunEnemy() {
         this.HUD.useOilStun();
         this.playerFOV.stunEnemiesInFOV();
 
-        const radius = 3 * this.map.tileWidth; // Circle radius in pixels
+        const radius = 3 * this.map.tileWidth;
         let circle = this.add.graphics();
 
-        // Draw the gradient circle
         for (let i = radius; i > 0; i--) {
             let alpha = i / radius;
             circle.fillStyle(0xffe64e, alpha);
@@ -236,7 +209,7 @@ class Level_1 extends Phaser.Scene {
         this.tweens.add({
             targets: circle,
             alpha: { from: 0.1, to: 0 },
-            duration: 500, // Duration of the flash effect in milliseconds
+            duration: 500,
             onComplete: () => {
                 circle.destroy();
             }
@@ -258,11 +231,11 @@ class Level_1 extends Phaser.Scene {
 
     drawDebug() {
         this.debugGraphics.clear();
-        this.debugGraphics.lineStyle(1, 0xff0000, 1); // Red color for collision boxes
+        this.debugGraphics.lineStyle(1, 0xff0000, 1);
         this.wallLayer.renderDebug(this.debugGraphics, {
-            tileColor: null, // No color for tiles
-            collidingTileColor: new Phaser.Display.Color(255, 0, 0, 30), // Red color for colliding tiles
-            faceColor: new Phaser.Display.Color(0, 255, 0, 255) // Green color for face edges
+            tileColor: null,
+            collidingTileColor: new Phaser.Display.Color(255, 0, 0, 30),
+            faceColor: new Phaser.Display.Color(0, 255, 0, 255)
         });
 
         this.debugGraphics.strokeRect(
@@ -274,14 +247,13 @@ class Level_1 extends Phaser.Scene {
 
         this.playerFOV.drawFOVOutline(this.debugGraphics);
 
-        // Draw debug for each enemy in the group
         this.enemies.getChildren().forEach(enemy => {
             enemy.enemyInstance.renderDebug(this.debugGraphics);
         });
     }
 
     playerEnemyCollision(player, enemySprite) {
-        const enemy = enemySprite.enemyInstance; // Access the Enemy instance
+        const enemy = enemySprite.enemyInstance;
         if (enemy) {
             enemy.enemyAttack();
         }
@@ -289,27 +261,22 @@ class Level_1 extends Phaser.Scene {
 
     isTileTransparent(x, y) {
         const tile = this.groundLayer.getTileAt(x, y);
-        return tile && tile.index !== -1; // Example condition, adjust as needed
+        return tile && tile.index !== -1;
     }
 
     updateMinimap() {
-        const minimapWidth = 250; // Width of the minimap
-        const minimapHeight = 250; // Height of the minimap
+        const minimapWidth = 250;
+        const minimapHeight = 250;
         const scaleX = minimapWidth / this.map.widthInPixels;
         const scaleY = minimapHeight / this.map.heightInPixels;
 
-        // Clear previous minimap graphics
         this.minimapGraphics.clear();
 
-        // Draw the ground and walls layers onto the minimap based on fog of war
         this.map.layers.forEach(layer => {
             layer.data.forEach(row => {
                 row.forEach(tile => {
-                    // NOTE: uncomment out this line below to show full minimap (3/4)
-                    //if (tile.index !== -1) {
-                    // NOTE: comment out this line below to show full minimap (4/4)
-                    if (tile.index !== -1 && !this.fogOfWar[tile.y][tile.x]) { // Only draw non-empty and revealed tiles
-                        const color = layer.name === 'Ground' ? 0x888888 : 0xcccccc; // Differentiate wall and ground tiles
+                    if (tile.index !== -1 && !this.fogOfWar[tile.y][tile.x]) {
+                        const color = layer.name === 'groundLayer' ? 0x888888 : 0xcccccc;
                         this.minimapGraphics.fillStyle(color, 1);
                         this.minimapGraphics.fillRect(tile.pixelX * scaleX, tile.pixelY * scaleY, scaleX * tile.width, scaleY * tile.height);
                     }
@@ -317,11 +284,9 @@ class Level_1 extends Phaser.Scene {
             });
         });
 
-        // Define a scale factor for the player on the minimap
         const playerMinimapScale = 2.5;
 
-        // Draw the player onto the minimap
-        this.minimapGraphics.fillStyle(0x00ff00, 1); // Green for the player
+        this.minimapGraphics.fillStyle(0x00ff00, 1);
         this.minimapGraphics.fillRect(
             (this.player.x * scaleX) - ((scaleX * this.player.width * (playerMinimapScale - 1)) / 2), 
             (this.player.y * scaleY) - ((scaleY * this.player.height * (playerMinimapScale - 1)) / 2), 
@@ -329,12 +294,10 @@ class Level_1 extends Phaser.Scene {
             scaleY * this.player.height * playerMinimapScale
         );
 
-        // For Debug. Hide this when using revealed minimap.
         const bookMinimapScale = 1;
-        // Draw the books onto the minimap with a smaller scale
         this.books.forEach(book => {
             book.setOrigin(0.5);
-            this.minimapGraphics.fillStyle(0x0000ff, 1); // Blue for the books
+            this.minimapGraphics.fillStyle(0x0000ff, 1);
             this.minimapGraphics.fillRect(
                 (book.x * scaleX) - ((scaleX * book.width * (bookMinimapScale - 1)) / 2),
                 (book.y * scaleY) - ((scaleY * book.height * (bookMinimapScale - 1)) / 2),
@@ -343,12 +306,11 @@ class Level_1 extends Phaser.Scene {
             );
         });
 
-        // Trigger an event to update the HUD with the new minimap graphics
         this.scene.get('hudScene').events.emit('updateMinimap', this.minimapGraphics);
     }
 
     revealMinimap() {
-        const revealRadius = 10; // Radius around the player to reveal tiles
+        const revealRadius = 10;
         const playerTileX = Math.floor(this.player.x / this.map.tileWidth);
         const playerTileY = Math.floor(this.player.y / this.map.tileHeight);
         for (let y = -revealRadius; y <= revealRadius; y++) {
@@ -356,10 +318,9 @@ class Level_1 extends Phaser.Scene {
                 const revealX = playerTileX + x;
                 const revealY = playerTileY + y;
                 if (revealX >= 0 && revealX < this.map.width && revealY >= 0 && revealY < this.map.height) {
-                    this.fogOfWar[revealY][revealX] = false; // Reveal the tile
+                    this.fogOfWar[revealY][revealX] = false;
                 }
             }
         }
     }
-
 }
