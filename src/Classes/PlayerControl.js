1ef509e1cb0a7002ec30d9ca424303c25507760c
Luan Ta <146981556+LuanTa101@users.noreply.github.com>
2024-06-12 11:56:54 -0700

    added some map details

--- src/Classes/PlayerControl.js
+++ src/Classes/PlayerControl.js
@@ -144,7 +144,7 @@ class PlayerControl {
     }
 
     update() {
-        const speed = 100;
+        const speed = 125;
         const player = this.player;
         const cursors = this.cursors;
         const keys = this.keys;
